2a53c1d07f0016bb4a77da7d325136941538bda3
Tyler <tyler@ogdenstudios.xyz>
2018-09-09 22:53:50 -0600

    update show for recipe. up to 57:13 in tutorial

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,6 +43,7 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :image)
+    params.require(:recipe).permit(:title, :description, :image, ingredientes_attributes: [:id, :name, :_destroy],
+    directions_attributes: [:id, :step, :_destroy])
   end
 end
