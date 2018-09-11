116257b1ca57c2919c42682dd9b50cbaf055c3e2
Tyler <tyler@ogdenstudios.xyz>
2018-09-11 00:08:52 -0600

    add associations for recipes and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910043038) do
+ActiveRecord::Schema.define(version: 20180911060713) do
 
   create_table "directions", force: :cascade do |t|
     t.text "step"
@@ -39,6 +39,18 @@ ActiveRecord::Schema.define(version: 20180910043038) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
