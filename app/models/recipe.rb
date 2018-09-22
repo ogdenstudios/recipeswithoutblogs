d562f787a9f907313e2c5d7ef001cfafd3395060
Tyler <tyler@ogdenstudios.xyz>
2018-09-22 02:00:14 -0600

    fix model validation so we can actually save new recipes without paperclip

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,6 @@ class Recipe < ApplicationRecord
     belongs_to :user
   	has_many :ingredients
   	has_many :directions
-    has_one_attached :image
 
   	accepts_nested_attributes_for :ingredients,
     															reject_if: proc { |attributes| attributes['name'].blank? },
@@ -11,6 +10,6 @@ class Recipe < ApplicationRecord
     															reject_if: proc { |attributes| attributes['step'].blank? },
     															allow_destroy: true
 
-    validates :title, :description, :image, presence: true
+    validates :title, :description, presence: true
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -57,10 +57,6 @@ ActiveRecord::Schema.define(version: 20180911175651) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
