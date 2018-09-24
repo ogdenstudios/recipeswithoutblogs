6898617f6411b4a1fdebc76f645c5559e044790f
Tyler <tyler@ogdenstudios.xyz>
2018-09-23 22:15:08 -0600

    add comment with image uploading link in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ApplicationRecord
   	has_many :directions
     has_one_attached :image
 
+    # For images, we'll go with this: https://www.railstutorial.org/book/user_microposts#sec-basic_image_uploads
+
   	accepts_nested_attributes_for :ingredients,
     															reject_if: proc { |attributes| attributes['name'].blank? },
     															allow_destroy: true
