6c48312148ef248e693abf76aff9c2aa67598790
Tyler Williams <tyler@ogdenstudios.xyz>
2019-02-11 15:09:23 -0700

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create seed users - initial email addresses are kept in an untracked file to preserve privacy
+
+# This will trigger an error if you're seeding the database without the user seed data
+begin
+    require_relative "data/users.rb"
+    SecretUserSeeds.emails.each do |email| 
+        User.create!(
+            email: email,
+            password: ENV["RWB_USER_PASSWORD"]
+        )
+    end
+rescue LoadError
+    puts "You don't have the secret user data - request it from tyler@ogdendstudios.xyz"
+    puts "Or, create your own dummy file at db/data/users.rb, if you just need to develop"
+    exit
+end
