7b378ed4198e88b082b02cd7e82d4d5ddad92c6e
Tyler Williams <tyler@ogdenstudios.xyz>
2019-02-16 14:06:03 -0700

    update devise action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Devise config 
+  config.action_mailer.default_url_options = { :host => 'recipeswithoutblogs.heroku.com' }
 
   # Devise Mailer Setup 
   config.action_mailer.delivery_method = :smtp
