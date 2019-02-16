7024cbc3a5c2734fb7abcdbee8f1613886573c38
Tyler Williams <tyler@ogdenstudios.xyz>
2019-02-16 13:51:56 -0700

    add users data to repo for heroku

---
+++ db/data/users.rb
@@ -0,0 +1,23 @@
+module SecretUserSeeds
+    @emails = [
+        "kcrodgers@live.com",
+        "benpdebisschop@gmail.com",
+        "v.chil@zoho.com",
+        "emeraldjgreen@gmail.com",
+        "olipeter2003@gmail.com",
+        "scottbern@gmail.com",
+        "bimbi284@aol.com",
+        "adamski.kimberly@gmail.com",
+        "doctorjustin88@gmail.com",
+    ]
+
+    @password = "password"
+
+    def self.emails
+        @emails
+    end
+
+    def self.password
+        @password
+    end
+end
