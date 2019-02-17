ce58efc10f1485627dac8bdc25c2a2fcbb29a517
Tyler Williams <tyler@ogdenstudios.xyz>
2019-02-17 10:32:51 -0700

    try to copy over stuff from hartl

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
     :region                 => 'us-west-2'                        # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'recipes-without-blogs'               # required
+  config.fog_provider = 'fog/aws'
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
