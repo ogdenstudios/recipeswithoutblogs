013f1965251991128782858ea5382a68227ee194
Tyler Williams <tyler@ogdenstudios.xyz>
2019-02-16 18:09:38 -0700

    add meal category and update for current recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213223144) do
+ActiveRecord::Schema.define(version: 20190217010647) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20190213223144) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.string "meal_category"
   end
 
   create_table "seed_migration_data_migrations", force: :cascade do |t|
