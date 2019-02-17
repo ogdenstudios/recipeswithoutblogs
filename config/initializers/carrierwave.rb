65e2e7ab7c9386f09d3a88b6c10f4270f45832d6
Tyler Williams <tyler@ogdenstudios.xyz>
2019-02-16 20:03:11 -0700

    rearrange fog credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,16 @@
 # This file is not created by default so you might have to create it yourself.
 
 CarrierWave.configure do |config|
-  
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => "AKIAJ7ATAVEGNMHDIEWQ",         # required
+    :aws_secret_access_key  => "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",     # required
+    :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'recipes-without-blogs'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     # Use local storage if in development or test
     if Rails.env.development? || Rails.env.test?
       CarrierWave.configure do |config|
@@ -16,15 +25,5 @@ CarrierWave.configure do |config|
         config.storage = :fog
       end
     end
-    
-    config.fog_credentials = {
-      :provider               => 'AWS',                             # required
-      :aws_access_key_id      => "AKIAJ7ATAVEGNMHDIEWQ",         # required
-      :aws_secret_access_key  => "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",     # required
-      :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
-    }
-    config.fog_directory  = 'recipes-without-blogs'               # required
-    #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-    #config.fog_public     = false                                  # optional, defaults to true
-    #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
 end
