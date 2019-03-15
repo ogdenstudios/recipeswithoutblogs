1cb4a4180ec3a62c0c5bbb38f036db1ba1af41e6
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-15 10:31:27 -0600

    remove turbolinks

--- app/assets/javascripts/orderSteps.js
+++ app/assets/javascripts/orderSteps.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (document.getElementsByClassName("edit_recipe").length > 0) {
-        console.log("Edit page scripts");
+        console.log("Edit view script started");
     }
 });
