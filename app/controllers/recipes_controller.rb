8a99765c4dd3dcb54b0d7702f3820af46c9c0fce
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-15 10:22:27 -0600

    allow order to be passed in through recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -58,7 +58,7 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :meal_category, :picture, :slug, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+    params.require(:recipe).permit(:title, :description, :meal_category, :picture, :slug, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :order, :_destroy])
   end
 
   def correct_user
