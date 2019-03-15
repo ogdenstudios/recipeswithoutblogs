823187548ee6d2f063305850bf59abb758e7a717
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-15 16:34:02 -0600

    add some placeholder specs for recipe and directions

---
+++ spec/models/direction_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Direction, :type => :model do 
+    it "is valid with valid attributes" 
+    it "is not valid without a step"
+    it "is not valid without an order"
+end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, :type => :model do 
+    it "is valid with valid attributes" 
+end
