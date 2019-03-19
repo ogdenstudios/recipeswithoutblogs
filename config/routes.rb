caa4b8d52803d86f8d99cdbd93ae1a81fba19945
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-19 16:23:27 -0600

    add readme note about testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "recipes#index"
-
   get "/about", to: "pages#about"
   get "/privacy-policy", to: "pages#privacy_policy"
   get "/recipes/mine", to: "recipes#mine"
