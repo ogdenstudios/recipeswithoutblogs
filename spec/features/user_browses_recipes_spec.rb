d9a0f90afb209302615b53d41c2fb2cdcc928bc0
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-20 15:42:41 -0600

    add scenarios where users try different meal types

--- spec/features/user_browses_recipes_spec.rb
+++ spec/features/user_browses_recipes_spec.rb
@@ -1,8 +1,36 @@
 require "rails_helper"
 
 RSpec.feature 'Recipe browsing', :type => :feature do
-    scenario 'they can see the homepage' do
+    scenario 'they can view the homepage' do
         visit root_path
         expect(page).to have_text 'Recipes'
+    end
+
+    scenario 'they can view breakfast recipes' do 
+        visit recipes_meal_path(slug: 'breakfast')
+        within('h1') do 
+            expect(page).to have_content 'Breakfast'
+        end
+    end
+    
+    scenario 'they can view lunch recipes' do 
+        visit recipes_meal_path(slug: 'lunch')
+        within('h1') do 
+            expect(page).to have_content 'Lunch'
+        end
+    end
+
+    scenario 'they can view dinner recipes' do 
+        visit recipes_meal_path(slug: 'dinner')
+        within('h1') do 
+            expect(page).to have_content 'Dinner'
+        end
+    end
+
+    scenario 'they can view other recipes' do 
+        visit recipes_meal_path(slug: 'other')
+        within('h1') do 
+            expect(page).to have_content 'Other'
+        end
     end
 end
