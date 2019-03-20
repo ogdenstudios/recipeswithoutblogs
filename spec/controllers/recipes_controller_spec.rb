55a15747299076631eb3f0423142d4e92c548b01
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-20 13:42:40 -0600

    add GET new controller tests

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe RecipesController, :type => :controller do
             expect(response).to render_template("index")
         end
     end
-
     describe "GET show" do 
         it "has a 200 status code" do
             recipe = create(:recipe)
@@ -33,6 +32,32 @@ RSpec.describe RecipesController, :type => :controller do
             recipe = create(:recipe)
             get :show, params: { id: recipe.id }
             expect(response).to render_template("show")
+        end
+    end
+    describe "GET new" do 
+        it "responds with a 302 status code if not logged in" do
+            get :new
+            expect(response.status).to eq(302)
+        end
+        it "redirects to the sign in page if not logged in" do
+            get :new
+            expect(response).to redirect_to('/users/sign_in')
+        end
+        it "has a 200 status code if logged in" do
+            user = create(:user)
+            sign_in user
+            get :new
+            expect(response.status).to eq(200)
+        end
+        it "responds with html by default" do 
+            get :new
+            expect(response.content_type).to eq "text/html"
+        end
+        it "renders new template if logged in" do 
+            user = create(:user)
+            sign_in user
+            get :new
+            expect(response).to render_template("new")
         end
     end
 end
