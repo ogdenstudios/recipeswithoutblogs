e020bb6e8aca27aa8bc642094b5a7b7df0ace7a3
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-20 14:17:34 -0600

    refactor recipe tests with more compact syntax

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,44 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Recipe, :type => :model do 
 
-    it "is valid with valid attributes" do 
+    it "has a valid factory" do 
         recipe = create(:recipe)
         expect(recipe).to be_valid 
     end
 
     it "is not valid without a user" do 
-        recipe = create(:recipe)
-        recipe.user_id = nil
-        expect(recipe).to_not be_valid
+        expect(build(:recipe, user_id: nil)).to_not be_valid
     end
 
     it "is not valid without a title" do
-        recipe = create(:recipe) 
-        recipe.title = nil
-        expect(recipe).to_not be_valid
+        expect(build(:recipe, title: nil)).to_not be_valid
     end
 
     it "is not valid without a description" do 
-        recipe = create(:recipe)
-        recipe.description = nil
-        expect(recipe).to_not be_valid
+        expect(build(:recipe, description: nil)).to_not be_valid
     end
 
     it "is not valid with a description longer than 280 characters" do 
-        recipe = create(:recipe)
-        recipe.description = (0...300).map { (rand(26).chr) }
-        expect(recipe).to_not be_valid
+        expect(build(:recipe, description: (0...300).map { (rand(26).chr) })).to_not be_valid
     end
 
     it "is not valid without a picture" do 
-        recipe = create(:recipe)
-        recipe.picture = nil
-        expect(recipe).to_not be_valid
+        expect(build(:recipe, picture: nil)).to_not be_valid
     end
 
     it "is not valid without a meal category" do 
-        recipe = create(:recipe)
-        recipe.meal_category = nil
-        expect(recipe).to_not be_valid
+        expect(build(:recipe, meal_category: nil)).to_not be_valid
     end
 end
