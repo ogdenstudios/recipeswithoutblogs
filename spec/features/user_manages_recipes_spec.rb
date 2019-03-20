a143caa00f89bf66cceff36124df269efe22fcd7
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-20 16:16:42 -0600

    add 50% of user recipe management spec"

--- spec/features/user_manages_recipes_spec.rb
+++ spec/features/user_manages_recipes_spec.rb
@@ -1,8 +1,31 @@
 require "rails_helper"
-
 RSpec.feature 'Recipe management', :type => :feature do
-    scenario 'they can add a recipe'
-    scenario 'they can view their recipes'
+    scenario 'they cannot view the new recipe form without being signed in' do 
+        visit new_recipe_path
+        expect(current_path).to eq '/users/sign_in'
+    end
+    scenario 'they can view the new recipe form when signed in' do 
+        user = create(:user)
+        visit new_user_session_path 
+        fill_in('user_email', :with => user.email)
+        fill_in('user_password', :with => user.password)
+        click_button('Log in')
+        visit new_recipe_path
+        expect(current_path).to eq '/recipes/new'
+    end
+    scenario 'they cannot view their recipes without being signed in' do 
+        visit recipes_mine_path 
+        expect(current_path).to eq '/users/sign_in'
+    end
+    scenario 'they can view their recipes' do 
+        user = create(:user) 
+        login_as(user)
+        visit recipes_mine_path 
+        expect(current_path).to eq '/recipes/mine'
+    end
+    scenario 'they cannot update a recipe without being signed in'
+    scenario "they cannot update another user's recipes"
     scenario 'they can update their recipes'
+    scenario "they cannot delete another user's recipes"
     scenario 'they can delete their recipes'
 end
