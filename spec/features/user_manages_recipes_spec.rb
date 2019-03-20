0c95a6d0b2b55d4ee489ce39c80e6b3b586c904f
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-20 15:59:48 -0600

    add blank scenarios to test for recipe management

---
+++ spec/features/user_manages_recipes_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.feature 'Recipe management', :type => :feature do
+    scenario 'they can add a recipe'
+    scenario 'they can view their recipes'
+    scenario 'they can update their recipes'
+    scenario 'they can delete their recipes'
+end
