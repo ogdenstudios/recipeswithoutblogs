673c4f0c36187ea151f60bbd1f7d45fcb6e099c5
Tyler Williams <tyler@ogdenstudios.xyz>
2019-03-21 08:54:35 -0600

    change feature phrasing to be about what users can see in page vs. controller actions

--- spec/features/user_manages_recipes_spec.rb
+++ spec/features/user_manages_recipes_spec.rb
@@ -23,32 +23,29 @@ RSpec.feature 'Recipe management', :type => :feature do
         visit recipes_mine_path 
         expect(current_path).to eq '/recipes/mine'
     end
-    scenario 'they cannot update a recipe without being signed in' do 
+    scenario 'they cannot visit a recipe edit page without being signed in' do 
         visit '/recipes/1/edit'
         expect(current_path).to eq '/users/sign_in'
     end
-    scenario "they cannot update another user's recipes" do 
+    scenario "they cannot see another user's recipes edit pages" do 
         user = create(:user) 
         login_as(user)
         visit '/recipes/1/edit'
         expect(current_path).to eq '/'
     end
-    scenario 'they can update their recipes' do 
+    scenario 'they can access their recipes edit page' do 
         user = create(:user)
         recipe = create(:recipe, user_id: user.id)
         login_as(user)
         visit "/recipes/#{recipe.id}/edit"
         expect(current_path).to eq "/recipes/#{recipe.id}/edit"
     end
-    scenario 'they can delete their recipes', :js => true do
+    scenario 'they can see a delete link on their recipe edit page' do
         user = create(:user)
         recipe = create(:recipe, user_id: user.id)
         login_as(user)
         visit "/recipes/mine"
         click_on(recipe.title)
-        accept_alert do 
-            click_on("Delete")
-        end
-        expect(Recipe.find(recipe.id)).to eq nil
+        expect(page).to have_content ('Delete')
     end
 end
